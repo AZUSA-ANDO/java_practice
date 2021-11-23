5969424b6cc68631f60502336ab40464b1972dd5
EC2 Default User <AZUSA-ANDO>
2021-11-23 15:23:10 +0000

    24日学習分

--- Chapter06.java
+++ Chapter06.java
@@ -8,4 +8,20 @@ c = a / 6;  // c = 2
 c = a % 4;  // c = 3
 
 
+String a = "applePie";
+String b = "applePie";
+c = a == b;  // c = true
 
+String a = "applePie";
+String d = "apple";
+String e = d + "Pie";
+c = a == e;  // c = false
+
+String a = "applePie";
+String f = new String("applePie");
+c = a == f;  // c = false
+
+// 変数の中身はすぐ上の例と同じ
+c = a.equals(b);  // c = true
+c = a.equals(e);  // c = true
+c = a.equals(f);  // c = true
